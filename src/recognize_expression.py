603ba9c91c101096b4b8579c65c418087b70bb2d
Josef Adamek <pepino.adamek@gmail.com>
2019-12-17 17:53:51 +0100

    Typo

--- src/recognize_expression.py
+++ src/recognize_expression.py
@@ -2,7 +2,7 @@
 Program for detecting eye and mouth expressions.
 
 USE WITH:
-    python facial_landmarks.py
+    python recognize_expression.py
 
 Pretrained facial landmarks detector can be downloaded here:
 - http://dlib.net/files/shape_predictor_68_face_landmarks.dat.bz2
